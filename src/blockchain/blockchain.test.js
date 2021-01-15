45c11ea920821e85035aaecc824171d7fc202f0c
Daniel Salvat <daniel.salvat@gmail.com>
2021-01-15 11:44:00 +0100

    P2P Service in Progress

---
+++ src/blockchain/blockchain.test.js
@@ -0,0 +1,58 @@
+import Blockchain from "./blockchain";
+import Block from "./block";
+import validate from "./modules/validate";
+
+describe('Blockchain', () => {
+    let blockchain;
+    let blockchainB;
+
+    beforeEach(() => {
+        blockchain = new Blockchain();
+        blockchainB = new Blockchain();
+    });
+
+    it('every blockchain contains a genesis block', () => {
+       const [genesisBlock] = blockchain.blocks;
+
+       expect(genesisBlock).toEqual(Block.genesis);
+       expect(blockchain.blocks.length).toEqual(1);
+    });
+
+    it('use addBlock()', () => {
+       const data =  'd4t4';
+       blockchain.addBlock(data);
+
+       const [, lastBlock] = blockchain.blocks;
+       expect(lastBlock.data).toEqual(data);
+       expect(blockchain.blocks.length).toEqual(2);
+
+    });
+
+    it('replace the chain with a valid chain', () => {
+        blockchainB.addBlock('bl4ck-1');
+        blockchain.replace(blockchainB.blocks);
+
+        expect(blockchain.blocks).toEqual(blockchainB.blocks);
+    });
+
+    it('cannot replace the chain with one with less blocks', () => {
+        blockchain.addBlock('bl4ck-1');
+
+        expect(() => {
+            blockchain.replace(blockchainB.blocks);
+        }).toThrowError('Received chain is not longer than current chain.');
+
+    });
+
+    it('cannot replace the chain with an invalid one', () => {
+        blockchainB.addBlock('bl4ck-1');
+        blockchainB.blocks[1].data = 'h4ck';
+
+        expect(() => {
+            blockchain.replace(blockchainB.blocks);
+        }).toThrowError('Received chain is invalid');
+
+
+    });
+
+});
